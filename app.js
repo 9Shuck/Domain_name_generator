9c31c6fbb8641aad4f100a4056249a82fc28e29e
Rubén Pérez Herrando <54080189+9Shuck@users.noreply.github.com>
2021-07-19 12:36:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,20 +38,4 @@ function checkExtension(domain, extension) {
 
     return domain.includes(extension, domain-length - extension.length);
 }
-=======
-let pronoun = ['the', 'our', 'his', 'her', 'mine', 'their', 'them'];
-let adjective = ['great', 'big', 'small', 'tall' , 'old', 'young', 'beautiful', 'nice'];
-let noun = ['dog', 'cat', 'ball', 'bird', 'cow', 'horse', 'sheep', 'racoon', 'bee'];
 
-let result = '';
-
-for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adjective.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-            result += pronoun[i] + adjective[j] + noun[k] + '.com' + '\n';
-        }
-    }
-    console.log(result);
-}
-
->>>>>>> 10307de6656cd921e1d6a21af7d5ec385f559fe2
